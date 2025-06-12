602fa6fcb4fa708ac54939d923c10c92ddf47e98
akanshathosar <akanshathosar@gmail.com>
2025-06-12 16:34:56 +0530

    Initial commit

---
+++ navigation.js
@@ -0,0 +1,7 @@
+function openMenu() {
+  document.getElementById("myMenu-responsive").style.width = "200px";
+}
+
+function closeMenu() {
+  document.getElementById("myMenu-responsive").style.width = "0";
+}
---
+++ typing.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typing', {
+    strings: ["Web Developer", "Data Analyst", "Data Scientist"], 
+    typeSpeed: 65, 
+    backSpeed: 65,
+    loop: true
+});
+
