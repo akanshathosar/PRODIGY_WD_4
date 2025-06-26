9c3fecfa41157fa7ccb02bca34cf9a41c0f5790b
akanshathosar <akanshathosar@gmail.com>
2025-06-26 12:13:23 +0530

    Initial commit

--- typing.js
+++ typing.js
@@ -5,3 +5,17 @@ var typed = new Typed('.typing', {
     loop: true
 });
 
+ScrollReveal().reveal('#about h2, #about p, #about img, #about .row, #about .interest-container, #about .interest-grid', {
+  delay: 200,
+  distance: '30px',
+  origin: 'bottom',
+  duration: 1000,
+  interval: 150,
+});
+
+new Typed(".typing-about", {
+  strings: ["Data Analyst", "Web Developer", "Tech Enthusiast"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true
+});
